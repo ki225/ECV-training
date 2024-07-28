08ce7593daadfab485b88c5b01f3d925792e0a2b
Kiki Huang <271yeye@gmail.com>
2024-07-28 18:13:12 +0800

    feat: using terraform to build DVWA

--- MSP/Building_DVWA_platform_by_using_AWS_ECS/terraform/ecs.tf
+++ MSP/Building_DVWA_platform_by_using_AWS_ECS/terraform/ecs.tf
@@ -6,16 +6,20 @@ resource "aws_ecs_cluster" "main" {
 # Task Definition
 resource "aws_ecs_task_definition" "dvwa" {
   family                   = "dvwa"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
+  /*
+  creating ECS Service (my-service): operation error ECS: CreateService, https response error StatusCode: 400, RequestID: 475a59bf-c562-4890-abc1-320bae82358c, InvalidParameterException: The provided target group arn:aws:elasticloadbalancing:us-east-1:429555954826:targetgroup/main-tg/f3e27a2ceae781be has target type instance, which is incompatible with the awsvpc network mode specified in the task definition.
+  */
+  network_mode             = "bridge" # https://stackoverflow.com/questions/60502112/ecs-target-type-ip-is-incompatible-with-the-bridge-network-mode-specified-in-t
+  requires_compatibilities = ["EC2"]
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = aws_iam_instance_profile.ssm_profile.arn
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
   container_definitions = jsonencode([
     {
       name  = "dvwa"
       image = "vulnerables/web-dvwa:latest"
+      essential = true
       portMappings = [
         {
           containerPort = 80
@@ -26,23 +30,50 @@ resource "aws_ecs_task_definition" "dvwa" {
   ])
 }
 
-
-
 # ECS Service
 resource "aws_ecs_service" "main" {
   name            = "my-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.dvwa.arn
   desired_count   = 1
-  launch_type     = "FARGATE"
+  launch_type     = "EC2"
 
-  network_configuration {
-    subnets = [aws_subnet.subnet_1.id]
-    assign_public_ip = true
-  }
+  // 當使用 bridge network mode 時，不需要在 aws_ecs_service 資源中指定 network_configuration。相反，當使用 awsvpc network mode 時，必須指定 network_configuration。
+  # network_configuration {
+  #   subnets = [aws_subnet.subnet_1.id]
+  #   security_groups = [aws_security_group.sg.id] 
+  #   assign_public_ip = true
+  # }
   load_balancer {
     target_group_arn = aws_lb_target_group.main.arn
-    container_name   = "my-container"
+    container_name   = "dvwa" # name should be right
     container_port   = 80
   }
+  # depends_on = [aws_lb_target_group.main]
+  # deployment_minimum_healthy_percent = 50
+  # deployment_maximum_percent         = 200
+  # lifecycle {
+  #   ignore_changes = [task_definition]
+  # }
+}
+
+resource "aws_instance" "ecs_instance" {
+  ami           = "ami-09aa9497baa9dbca3" # amzn2-ami-ecs-hvm-2.0.20240723-x86_64-ebs
+  instance_type = "t2.micro"
+  key_name      = "kp"
+  subnet_id     = aws_subnet.subnet_1.id 
+  vpc_security_group_ids = [aws_security_group.sg.id]  
+
+  tags = {
+    Name = "ecs-instance"
+  }
+
+  # 安裝 ECS agent
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt-get update -y
+                sudo apt-get upgrade -y
+                sudo docker pull vulnerables/web-dvwa
+                sudo docker run -d -p 80:80 vulnerables/web-dvwa
+                EOF
 }
--- MSP/Building_DVWA_platform_by_using_AWS_ECS/terraform/iam.tf
+++ MSP/Building_DVWA_platform_by_using_AWS_ECS/terraform/iam.tf
@@ -1,24 +1,101 @@
+# resource "aws_iam_role" "ecs_task_execution" {
+#   name = "ec2_ssm_role"
+
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+       
+#         Action :[
+#                 "sts:AssumeRole",
+#                 # "sts:SetSourceIdentity",
+#                 # "sts:TagSession",
+#                 # "ecs:RegisterTaskDefinition",
+#                 # "ecs:ListTaskDefinitions",
+#                 # "ecs:DescribeTaskDefinition",
+#                 # "iam:CreateRole",
+#                 # "iam:PutRolePolicy",
+#                 # "iam:GetRole",
+#                 # "iam:AttachRolePolicy",
+#                 # "iam:PassRole",
+                
+#               ]
+#         Effect = "Allow"
+#         "Sid": "AllowCreateRoles"
+#         Principal = {
+#             "Service": [
+#                   # "ecs.amazonaws.com",
+#                   "application-autoscaling.amazonaws.com",
+#                   "ecs-tasks.amazonaws.com"
+#             ]
+#         }
+#       }
+#     ]
+#   })
+# }
+
+#  /* 
+#         problem: creating ECS Task Definition (): operation error ECS: RegisterTaskDefinition
+#         solution:
+#         https://stackoverflow.com/questions/68873088/authorization-error-in-deploy-aws-ecs-task-definition-via-github-actions
+#         */
+#         // sts:AssumeRole needs to be in first position at the action attr.
+#         // https://github.com/hashicorp/terraform-provider-aws/issues/32872
+# resource "aws_iam_role_policy" "ecs_task_definition_policy" {
+#   name = "ecsTaskDefinitionPolicy"
+#   role = aws_iam_role.ecs_task_execution.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Action = [
+#           "ecs:RegisterTaskDefinition",
+#           "ecs:ListTaskDefinitions",
+#           "ecs:DescribeTaskDefinition",
+#           "iam:CreateRole"
+#         ],
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+
+
+# resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+#   role       = aws_iam_role.ecs_task_execution.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
+
+# resource "aws_iam_role_policy_attachment" "ecs_ssm_policy" {
+#   role       = aws_iam_role.ecs_task_execution.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+# }
+
+# # Create an instance profile
+# resource "aws_iam_instance_profile" "ssm_profile" {
+#   name = "ec2_ssm_profile"
+#   role = aws_iam_role.ecs_task_execution.name
+# }
+
+
 resource "aws_iam_role" "ecs_task_execution" {
-  name = "ec2_ssm_role"
+  name = "ecs_task_execution_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Effect = "Allow",
         Principal = {
-            "Service": [
-                  "ecs.amazonaws.com",
-                  "application-autoscaling.amazonaws.com",
-                  "ecs-tasks.amazonaws.com"
-            ]
-        }
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
       }
     ]
   })
 }
-
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   role       = aws_iam_role.ecs_task_execution.name
@@ -28,10 +105,4 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
 resource "aws_iam_role_policy_attachment" "ecs_ssm_policy" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-# Create an instance profile
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "ec2_ssm_profile"
-  role = aws_iam_role.ecs_task_execution.name
 }

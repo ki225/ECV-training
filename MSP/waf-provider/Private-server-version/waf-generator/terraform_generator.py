373722dc6c8ec3f6e72f550d1bf59777f4ae3465
Kiki Huang <271yeye@gmail.com>
2024-08-30 17:59:30 +0800

    feat: modify the code

--- MSP/waf-provider/Private-server-version/waf-generator/terraform_generator.py
+++ MSP/waf-provider/Private-server-version/waf-generator/terraform_generator.py
@@ -444,7 +444,9 @@ class WAFConfig(BaseModel):
 def generate_terraform(config: json) -> str:
     # config_data = json.loads(config)
     waf_config = TypeAdapter(WAFConfig).validate_python(config)
-    customer_credential = "arn:aws:iam::429555954826:role/customer-role"   # IAM role ARN
+
+    customer_credential = "<CUSTOMER_ROLE>"   # IAM role ARN
+
 
     terraform_config = f"""
     # AWS Provider
@@ -457,7 +459,6 @@ def generate_terraform(config: json) -> str:
     }}
 
     resource "aws_wafv2_web_acl" "{waf_config.Waf.Name}" {{
-        provider = aws.customer
         name        = "{waf_config.Waf.Name}"
         description = "{waf_config.Waf.Description}"
         scope       = "{'CLOUDFRONT' if waf_config.Resource.Type.upper() == 'CLOUDFRONT' else 'REGIONAL'}"
